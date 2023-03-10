a8ca1abb6825aeb16066c321bbf21331b4d64951
Adam <adam.zukowski471@gmail.com>
2023-03-10 18:00:59 +0100

    Add hideBooks. Task 10.2.5

--- src/js/script.js
+++ src/js/script.js
@@ -13,6 +13,9 @@
     itemBook: {
       image: '.book__image',
     },
+    filters: {
+      section: '.filters',
+    }
   };
   const templates = {
     book: Handlebars.compile(document.querySelector(select.templateOf.book).innerHTML),
@@ -35,11 +38,10 @@
   };
 
   const favoriteBooks = [];
-  
+  const filters = [];
   function initAction(){
     
-    //const clicker = document.querySelectorAll(select.itemBook.image);
-    //for(let item of clicker){
+
     const bookList = document.querySelector(select.booksPanel.booksList);
     bookList.addEventListener('dblclick', function(event){
       event.preventDefault();
@@ -62,10 +64,54 @@
         console.log('bookId: ', bookId);
         console.log('favorite: ', favoriteBooks);
       }
-      
     });
-    //}
+  
+    const filter = document.querySelector(select.filters.section);
+    filter.addEventListener('click', function(event){
+     
+      
+      if(event.target.tagName == 'INPUT' && event.target.type == 'checkbox' && event.target.name == 'filter'){
+        console.log('value: ', event.target.value);
+        if(event.target.checked == true){
+          filters.push(event.target.value);
+        } else {
+          const arrayFilterNumber = filters.indexOf(event.target.value);
+          filters.splice(arrayFilterNumber, 1);
+        }
+      }
+      hideBooks();
+      console.log('filters: ', filters);
+    });
   }
+
+  const hideBooks = function(){
+    for (let book of dataSource.books){
+      
+      let shouldBeHidden = false;
+      for (let filter of filters){
+        if(!book.details[filter]){
+          shouldBeHidden = true;
+          break;
+        }
+        //console.log('id: ', book.details[filter]);
+      }
+      console.log('dupa: ', book.id); 
+      const BookImg = document.querySelector('[data-id="' + book.id + '"]');
+      console.log('bimg: ', BookImg);
+      if(shouldBeHidden == true){
+        
+        
+        BookImg.classList.add('hidden');
+      } else {
+        BookImg.classList.remove('hidden');
+      }
+      
+      //console.log('bimg: ', book);
+    }
+    
+    
+  };
+
   render();
   initAction();
 }
